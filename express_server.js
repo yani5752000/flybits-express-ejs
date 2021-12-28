94afee56a41e626286370b2e8560f73dfb6a8f80
Farid <yani5752000@yahoo.com>
2021-12-28 17:09:42 -0500

    added test endpoints

--- express_server.js
+++ express_server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const database = {
+const flyDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/fly.json", (req, res) => {
+  res.json(flyDatabase);
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
