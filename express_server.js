53b4ed26f9a822871d787223da6caa505aeff684
Farid <yani5752000@yahoo.com>
2021-12-28 18:07:27 -0500

    installed ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const flyDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +18,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/user", (req, res) => {
+  res.render("userPage");
+});
+
+app.get("/marketer", (req, res) => {
+  res.render("marketerPage");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
