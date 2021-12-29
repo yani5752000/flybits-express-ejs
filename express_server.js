e4a2a6afd034df9feaa838ff50fbbba4e5b53472
Farid <yani5752000@yahoo.com>
2021-12-29 13:28:50 -0500

    added the databases

--- express_server.js
+++ express_server.js
@@ -10,10 +10,25 @@ const flyDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const branchDatabase = {
+  1: {id: 1, Latitude: 3, Longitude: 5},
+  2: {id: 2, Latitude: 8, Longitude: 2}
+};
+
+const promotionDatabase = {
+  1: {id: 1, 
+      branchId: 2, 
+      image: "https://www.shutterstock.com/image-illustration/3d-illustration-cartoon-characters-businessmen-hands-2022445304",
+      caption: "Great Opportunity"},
+  2:  {id: 1, 
+       branchId: 2, 
+       image: "https://www.shutterstock.com/image-vector/central-bank-money-policy-inflation-interest-1996866989",
+       caption: "Fantastic Opportunity"}
+};
 
 app.get("/fly.json", (req, res) => {
   res.json(flyDatabase);
-});
+}); 
 
 
 app.get("/", (req, res) => {
