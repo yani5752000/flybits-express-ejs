da102cf08321df24210c61f06b57e74c17cbee71
Farid <yani5752000@yahoo.com>
2021-12-28 19:04:22 -0500

    starting the pages

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + '/public'));
+
 const flyDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
