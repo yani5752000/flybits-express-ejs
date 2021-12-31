d0105dd4bc6dc85def37085ee516d77293af984b
Farid <yani5752000@yahoo.com>
2021-12-31 16:28:43 -0500

    started writing client.js

---
+++ public/scripts/client.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("ready");
+
+  // $("#tweet-text").on("input", function(event) {
+  //   console.log(140 - $(this).val().length);
+  //   let tweetLength = $(this).val().length;
+  //   let counter = $("#counter");
+  //   counter.val(140 - tweetLength);
+  //   if (counter.val() < 0) {
+  //     counter.css("color", "red");
+  //   } else {
+  //     counter.css("color", "#545149");
+  //   }
+  // });
+
+  $( ".marketer-div" ).click(function() {
+    alert( "Handler for .click() called." );
+  });
+});
