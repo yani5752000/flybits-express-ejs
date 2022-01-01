d772c7179cbcac7c7c065062ec7a68f0188ce025
Farid <yani5752000@yahoo.com>
2022-01-01 15:43:53 -0500

    migrated the js to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,44 @@
+
+function initMap() {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  // let infoWindow = new google.maps.InfoWindow({
+  //   content: "Click the map to get Lat/Lng!",
+  //   position: myLatlng,
+  // });
+
+  // infoWindow.open(map);
+  // // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    //mapsMouseEvent.latLng
+    alert( "Handler for .click() called." + mapsMouseEvent.latLng);
+    // // Close the current InfoWindow.
+    // infoWindow.close();
+    // // Create a new InfoWindow.
+    // infoWindow = new google.maps.InfoWindow({
+    //   position: mapsMouseEvent.latLng,
+    // });
+    // infoWindow.setContent(
+    //   JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    // );
+    // infoWindow.open(map);
+  });
+}
+
 $(document).ready(function() {
   // --- our code goes here ---
   console.log("ready");
+
+  // var dumbo = {lat: 40.700802, lng:73.987602};
+  // var mapOptions = {
+  //     center: dumbo,
+  //     zoom: 10
+  // };
+  // var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   // $("#tweet-text").on("input", function(event) {
   //   console.log(140 - $(this).val().length);
