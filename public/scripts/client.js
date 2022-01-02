8ad192eb5d1af0916e5acab71e80049e0800fb0e
Farid <yani5752000@yahoo.com>
2022-01-02 18:25:10 -0500

    added the map for marketer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,50 @@
+function initMarketerMap() {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const marketer_map = new google.maps.Map(document.getElementById("marketer-map"), {
+    zoom: 4,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  // let infoWindow = new google.maps.InfoWindow({
+  //   content: "Click the map to get Lat/Lng!",
+  //   position: myLatlng,
+  // });
+
+  // infoWindow.open(map);
+  // // Configure the click listener.
+  marketer_map.addListener("mousemove", (mapsMouseEvent) => {
+    //mapsMouseEvent.latLng
+    const lat1 =$("#latitude1");
+    const lng1 =$("#longitude1");
+    lat1.val("Your Latitude is " + mapsMouseEvent.latLng.lat());
+    lng1.val("Your Longitude is " + mapsMouseEvent.latLng.lng());
+  });
+  // map.addListener("click", (mapsMouseEvent) => {
+  //   //mapsMouseEvent.latLng
+  //   const lat =$("#latitude");
+  //   const lng =$("#longitude");
+  //   lat.val("Your Latitude is " + mapsMouseEvent.latLng.lat());
+  //   lng.val("Your Longitude is " + mapsMouseEvent.latLng.lng());
+  //   // alert( "Handler for .click() called. " 
+  //   // + mapsMouseEvent.latLng 
+  //   // + " " + mapsMouseEvent.latLng.lat()
+  //   // + " " + mapsMouseEvent.latLng.lng());
+  //   // // Close the current InfoWindow.
+  //   // infoWindow.close();
+  //   // // Create a new InfoWindow.
+  //   // infoWindow = new google.maps.InfoWindow({
+  //   //   position: mapsMouseEvent.latLng,
+  //   // });
+  //   // infoWindow.setContent(
+  //   //   JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+  //   // );
+  //   // infoWindow.open(map);
+  // });
+}
 
 function initMap() {
   const myLatlng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("user-map"), {
     zoom: 4,
     center: myLatlng,
   });
