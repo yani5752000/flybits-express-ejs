1f619d41987050d7da8fa0eb17db5981bb04c197
Farid <yani5752000@yahoo.com>
2022-01-09 15:35:36 -0500

    created helpers.js and transferred the code into it

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+const {flyDatabase, branchDatabase, promotionDatabase} = require("./helpers")
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -10,25 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 
-const flyDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-const branchDatabase = {
-  1: {id: 1, latitude: 3, longitude: 5},
-  2: {id: 2, latitude: 8, longitude: 2}
-};
 
-const promotionDatabase = {
-  1: {id: 1, 
-      branchId: 2, 
-      imageUrl: "/images/promotion_image.webp",
-      caption: "Great Opportunity"},
-  2:  {id: 1, 
-       branchId: 2, 
-       imageUrl: "/images/promotion_image.webp",
-       caption: "Fantastic Opportunity"}
-};
 
 app.get("/fly.json", (req, res) => {
   res.json(flyDatabase);
---
+++ helpers.js
@@ -0,0 +1,21 @@
+const flyDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+const branchDatabase = {
+  1: {id: 1, latitude: 3, longitude: 5},
+  2: {id: 2, latitude: 8, longitude: 2}
+};
+
+const promotionDatabase = {
+  1: {id: 1, 
+      branchId: 2, 
+      imageUrl: "/images/promotion_image.webp",
+      caption: "Great Opportunity"},
+  2:  {id: 1, 
+       branchId: 2, 
+       imageUrl: "/images/promotion_image.webp",
+       caption: "Fantastic Opportunity"}
+};
+
+module.exports = {flyDatabase, branchDatabase, promotionDatabase};
